e4da15987837eb865460e54779ef0023355ff4af
Viktor Kleen <viktor.kleen@tweag.io>
2023-06-27 13:19:55 +0000

    Finish draft-07 predicate combinators

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{error::Error, fs::File, io::stdout, path::PathBuf};
 use clap::Parser;
 use json_schema_to_nickel::schema_to_contract;
 use nickel_lang::pretty::*;
-use schemars::schema::Schema;
+use schemars::schema::{RootSchema, Schema};
 use terminal_size::{terminal_size, Width};
 
 #[derive(Parser)]
@@ -14,14 +14,15 @@ struct Args {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let f = File::open(PathBuf::from(Args::parse().schema))?;
-    let schema: Schema = serde_json::from_reader(f)?;
+    let root_schema: RootSchema = serde_json::from_reader(f)?;
 
     let size = terminal_size()
         .map(|(Width(w), _)| w as usize)
         .unwrap_or(80);
 
     let types: DocBuilder<'_, _, ()> =
-        dbg!(schema_to_contract(dbg!(schema))).pretty(&pretty::BoxAllocator);
+        dbg!(schema_to_contract(Schema::Object(dbg!(root_schema).schema)))
+            .pretty(&pretty::BoxAllocator);
 
     types.render(size, &mut stdout())?;
 
