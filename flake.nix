9d29b89563b21b74b66ab35aa097ddf4298838ad
Viktor Kleen <viktor.kleen@tweag.io>
2023-07-03 11:52:37 +0000

    Set up flake.nix build with crane

--- flake.nix
+++ flake.nix
@@ -3,54 +3,88 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
-    naersk = {
-      url = "github:nix-community/naersk";
+    crane = {
+      url = "github:ipetkov/crane";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nickel = {
       url = "github:tweag/nickel";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "utils";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     topiary.url = "github:tweag/topiary";
   };
 
-  outputs = { self, nixpkgs, utils, naersk, nickel, ... }@inputs: {
-    overlays = rec {
-      expects-naersk = final: _: {
-        json-schema-to-nickel = final.naersk.buildPackage {
-          pname = "json-schema-to-nickel";
-          root = ./.;
-        };
-      };
-
-      default = final: _: {
-        inherit (final.appendOverlays [
-          naersk.overlay
-          expects-naersk
-        ]) json-schema-to-nickel;
-      };
-    };
-  } // utils.lib.eachDefaultSystem (system: with import nixpkgs
-    { overlays = [ self.overlays.default ]; inherit system; }; {
-    packages.default = json-schema-to-nickel;
-
-    devShells.default = mkShell {
-      packages = [
-        cargo
-        cargo-watch
-        clippy
-        inputs.topiary.packages.${system}.default
-        nickel.packages.${system}.default
-        rust-analyzer
-        rustc
-        rustfmt
+  outputs = inputs:
+    let
+      SYSTEMS = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
       ];
-    };
-  });
+
+      lib = inputs.nixpkgs.lib;
+      foreach = xs: f: with lib; foldr recursiveUpdate { } (map f xs);
+      forSystems = systems: f: foreach systems (system: f system inputs.nixpkgs.legacyPackages.${system});
+    in
+    forSystems SYSTEMS (system: pkgs:
+      let
+        craneLib = inputs.crane.mkLib pkgs;
+
+        src =
+          let
+            mkFilter = regexp: path: _type: builtins.match regexp (lib.traceVal path) != null;
+          in
+          lib.cleanSourceWith {
+            src = lib.cleanSource (craneLib.path ./.);
+            filter = path: type:
+              builtins.any (filter: filter path type) [
+                (mkFilter "vendor/JSON-Schema-Test-Suite/tests/.*json$")
+                craneLib.filterCargoSources
+              ];
+          };
+
+        commonArgs = {
+          inherit src;
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        json-schema-to-nickel = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+        });
+      in
+      {
+        checks.${system} = {
+          json-schema-to-nickel-clippy = craneLib.cargoClippy (commonArgs // {
+            inherit cargoArtifacts;
+            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+          });
+
+          json-schema-to-nickel-fmt = craneLib.cargoFmt commonArgs;
+
+          json-schema-to-nickel-test = craneLib.cargoTest (commonArgs // {
+            inherit cargoArtifacts;
+          });
+
+          inherit (inputs.self.packages.${system}) json-schema-to-nickel;
+        };
+
+        packages.${system} = rec {
+          inherit json-schema-to-nickel;
+          default = json-schema-to-nickel;
+        };
+
+        devShells.${system}.default = pkgs.mkShell {
+          inputsFrom = lib.attrValues inputs.self.checks.${system};
+          packages = with pkgs; [
+            cargo-watch
+            inputs.topiary.packages.${system}.default
+            inputs.nickel.packages.${system}.default
+            rust-analyzer
+            rustfmt
+          ];
+        };
+      });
 }
