c9e91c961e06c7413ca8fd21006351145f642029
Joe Neeman <joeneeman@gmail.com>
2023-09-04 15:55:45 -0500

    Interpret '-' as stdin

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use std::{error::Error, fs::File, io::stdout, path::PathBuf};
+use std::{
+    error::Error,
+    fs::File,
+    io::{stdout, Read},
+    path::PathBuf,
+};
 
 use clap::Parser;
 use json_schema_to_nickel::root_schema;
@@ -9,11 +14,17 @@ use terminal_size::{terminal_size, Width};
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
+    /// Path to a JSON schema file, or "-" to read the schema file from stdin.
     schema: String,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let f = File::open(PathBuf::from(Args::parse().schema))?;
+    let args = Args::parse();
+    let f: Box<dyn Read> = if args.schema == "-" {
+        Box::new(std::io::stdin())
+    } else {
+        Box::new(File::open(PathBuf::from(args.schema))?)
+    };
     let schema: RootSchema = serde_json::from_reader(f)?;
 
     let size = terminal_size()
