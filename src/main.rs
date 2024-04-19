7ff5f2eb9ade9b1983053e590c3a883eb89fbff6
Yann Hamdaoui <yann.hamdaoui@tweag.io>
2024-04-19 12:17:54 +0200

    Add --library-path argument
    By default, the internal js2n predicate library is inlined in each
    generated contract to ensure that this is a standalone Nickel file that
    can be imported and moved around freely. However, this eats quite a lot
    of space, especially when a lot of contracts are generated and
    distributed together.
    
    This commits add a CLI argument, `--library-path`, to provide a path from
    which the generated contract should import the library, instead of
    inlining it, so that the library can be shared by many contracts.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::{
     error::Error,
+    ffi::OsString,
     fs::File,
     io::{stdout, Read},
 };
 
 use clap::Parser;
-use json_schema_to_nickel::root_schema;
+use json_schema_to_nickel::convert;
 use nickel_lang_core::pretty::*;
 use schemars::schema::RootSchema;
 use terminal_size::{terminal_size, Width};
@@ -15,6 +16,18 @@ use terminal_size::{terminal_size, Width};
 struct Args {
     /// Path to a JSON schema file. If omitted, the schema file will be read from stdin.
     schema: Option<String>,
+
+    /// Generate a contract that will import the json-schema-to-nickel Nickel library from the
+    /// given path. The path must point to the entry point of the library, currently
+    /// `predicates.ncl`.
+    ///
+    /// By default, when no library path is specified, json-schema-to-nickel will inline this
+    /// library inside the contract to make it standalone. However, when converting many schemas
+    /// and distributing them together, this inlining leads to a lot of code duplication. Using
+    /// this argument avoids the duplication, although it makes you responsible of making the
+    /// json-schema-to-nickel library available at the right place.
+    #[clap(long)]
+    library_path: Option<OsString>,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -31,7 +44,8 @@ fn main() -> Result<(), Box<dyn Error>> {
         .map(|(Width(w), _)| w as usize)
         .unwrap_or(80);
 
-    let types: DocBuilder<'_, _, ()> = root_schema(&schema).pretty(&pretty::BoxAllocator);
+    let types: DocBuilder<'_, _, ()> =
+        convert(&schema, args.library_path).pretty(&pretty::BoxAllocator);
 
     println!("# DO NOT EDIT\n# This file was automatically generated using json-schema-to-nickel");
     types.render(size, &mut stdout())?;
