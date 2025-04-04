975ee794acc9c7cd5b5531071c5bc1f9fb6fdeb8
Joe Neeman <joe.neeman@tweag.io>
2025-04-04 09:24:51 +0700

    Document typ

--- src/typ.rs
+++ src/typ.rs
@@ -1,8 +1,11 @@
+//! JSON types
+
 use std::str::FromStr;
 
 use crate::object::Obj;
 use crate::schema::{Arr, Num, Schema, Str};
 
+/// The six types in JSON.
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[repr(u8)]
 pub enum InstanceType {
@@ -15,11 +18,13 @@ pub enum InstanceType {
 }
 
 impl InstanceType {
+    /// Returns an array of all JSON types.
     pub fn all() -> [InstanceType; 6] {
         use InstanceType::*;
         [Null, Boolean, Object, Array, Number, String]
     }
 
+    /// Returns a schema that checks a type and nothing else.
     pub fn to_schema(self) -> Schema {
         match self {
             InstanceType::Null => Schema::Null,
@@ -50,35 +55,46 @@ impl FromStr for InstanceType {
     }
 }
 
+/// A set of JSON types.
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct InstanceTypeSet {
+    /// There are only six types, so we represent a set of them using a
+    /// bitfield.
     inner: u8,
 }
 
 impl InstanceTypeSet {
+    /// The set of all types.
     pub const FULL: InstanceTypeSet = InstanceTypeSet { inner: 0b0011_1111 };
+    /// The set of no types.
     pub const EMPTY: InstanceTypeSet = InstanceTypeSet { inner: 0b0000_0000 };
 
+    /// Constructs a set with one type in it.
     pub fn singleton(ty: InstanceType) -> Self {
         Self {
             inner: 1 << ty as u8,
         }
     }
 
+    /// Inserts a type into this set.
     pub fn insert(&mut self, ty: InstanceType) {
         self.inner |= 1 << ty as u8;
     }
 
+    /// Does this set contain `ty`?
     pub fn contains(self, ty: InstanceType) -> bool {
         self.inner & (1 << ty as u8) != 0
     }
 
+    /// Intersects this set with another one, returning the intersection.
     pub fn intersect(self, other: InstanceTypeSet) -> InstanceTypeSet {
         InstanceTypeSet {
             inner: self.inner & other.inner,
         }
     }
 
+    /// Creates a schema that validates whether a value belongs to any of the
+    /// types in this type set.
     pub fn to_schema(self) -> Schema {
         let types: Vec<_> = InstanceType::all()
             .into_iter()
@@ -92,6 +108,7 @@ impl InstanceTypeSet {
         }
     }
 
+    /// If this set is a singleton, returns the single type that it contains.
     pub fn to_singleton(self) -> Option<InstanceType> {
         if self.inner.count_ones() == 1 {
             InstanceType::all()
